117469ea0a5cb5c9fff48a3baacaa011a5878ac7
Mohamed Mankouchi <mohamed.mankouchi@springbokagency.com>
2025-01-16 10:31:12 +0100

    feat: fixed path router

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { router } from "./router/route.tsx";
+import { router } from "./router/route";
 import { RouterProvider } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 
